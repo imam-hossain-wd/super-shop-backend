09b86025c5ca1e666b67a65f0aa1c35c49ec27c2
imam-hossain1 <zunayedemon6t9@gmail.com>
2023-12-15 23:01:34 +0600

    user zod validation complete

--- src/app/modules/auth/auth.route.ts
+++ src/app/modules/auth/auth.route.ts
@@ -1,10 +1,18 @@
 import { Router } from "express";
 import { authController } from "./auth.controller";
+import validateRequest from "../../middlewares/validateRequest";
+import { AuthValidation } from "./auth.validation";
 
 
 const router = Router();
-router.post('/signup',authController.createUser);
-router.post('/login',authController.logInUser);
-router.post('/refresh-token',authController.refreshToken);
+router.post('/signup',
+validateRequest(AuthValidation.createUserZodSchema),
+authController.createUser);
+router.post('/login',
+validateRequest(AuthValidation.loginUserZodSchema),
+authController.logInUser);
+router.post('/refresh-token',
+validateRequest(AuthValidation.refreshTokenZodSchema),
+authController.refreshToken);
 
 export const authRoutes = router;
---
+++ src/app/modules/auth/auth.validation.ts
@@ -0,0 +1,43 @@
+import { z } from 'zod';
+
+const createUserZodSchema = z.object({
+  body: z.object({
+    name: z.string({
+      required_error: 'name is required',
+    }),
+    email: z.string({
+      required_error: 'email is required',
+    }),
+    password: z.string({
+      required_error: 'password is required',
+    }),
+  }),
+});
+
+const loginUserZodSchema = z.object({
+  body: z.object({
+    email: z.string({
+      required_error: 'email is required',
+    }),
+    password: z.string({
+      required_error: 'password is required',
+    })
+  }),
+});
+
+const refreshTokenZodSchema = z.object({
+  cookies: z.object({
+    refreshToken: z.string({
+      required_error: 'Refresh Token is required',
+    }),
+  }),
+});
+
+
+
+
+export const AuthValidation = {
+  createUserZodSchema,
+  loginUserZodSchema,
+  refreshTokenZodSchema
+};
